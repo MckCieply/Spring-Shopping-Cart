22f1ff6141c4e5f958355634fb6b559b4ee8d512
Alex <mwppl123@gmail.com>
2023-09-14 00:52:17 +0200

    Connecting and generating SQL

--- src/main/java/com/mckcieply/shoppingcart/item/Item.java
+++ src/main/java/com/mckcieply/shoppingcart/item/Item.java
@@ -1,5 +1,6 @@
 package com.mckcieply.shoppingcart.item;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,8 +10,12 @@ import lombok.ToString;
 @Setter
 @Getter
 @ToString
-
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private double price;
     private int quantity;
