b26f7a91386f55253f2fb4afc7d0d90707f7abc2
Alex <mwppl123@gmail.com>
2023-09-18 13:00:24 +0200

    Functionality of +/- buttons

--- src/main/resources/static/base.js
+++ src/main/resources/static/base.js
@@ -1,16 +1,19 @@
-let clicks = 0
-function generateClicks(){
-    let length = document.getElementById("lengthOfStock").value;
-    let tabOfClicks = new Array(length);
-    return tabOfClicks;
-}
-function addToCart(){
-    clicks += 1;
-    document.getElementById("inCart").innerHTML = clicks;
 
-}
-function removeFromCart(){
-    clicks -= 1;
-    document.getElementById("inCart").innerHTML = clicks;
+function generateIDs(){
+    const counters = document.querySelectorAll('.Counter');
+    counters.forEach((counter, index) => {
+    counter.id = `inCart${index + 1}`;
+    });
+};
+function changeCart(button, action){
+    let tr = button.parentNode.parentNode;
+    let td = tr.querySelector(".Counter")
+    let clicks = parseInt(td.innerText)
 
-}
+    if(action === "increment")
+        clicks += 1;
+    else if(action === "decrement" && clicks > 0)
+        clicks -= 1;
+    td.innerText = clicks.toString();
+
+};
