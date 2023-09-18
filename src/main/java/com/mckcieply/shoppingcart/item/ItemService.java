d8a1a7ff3daee8ffb21301b5891ee6b8a1c838e5
Alex <mwppl123@gmail.com>
2023-09-18 11:33:37 +0200

    getLength and getAll functions

--- src/main/java/com/mckcieply/shoppingcart/item/ItemService.java
+++ src/main/java/com/mckcieply/shoppingcart/item/ItemService.java
@@ -3,6 +3,8 @@ package com.mckcieply.shoppingcart.item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ItemService {
     @Autowired
@@ -11,5 +13,14 @@ public class ItemService {
     // Save item to database
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    // Get all items from database
+    public List<Item> getAll() {
+        return itemRepository.findAll();
+    }
+
+    public int getLength() {
+        return itemRepository.findAll().size();
     }
 }
