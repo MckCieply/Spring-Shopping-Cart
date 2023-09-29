551d91338cc03a0ca6cf8defc72aa4d31b9f4489
Alex <mwppl123@gmail.com>
2023-09-29 18:33:58 +0200

    Visual update

--- src/main/resources/static/base.js
+++ src/main/resources/static/base.js
@@ -1,13 +1,13 @@
 
 function generateIDs(){
-    const counters = document.querySelectorAll('.Counter');
+    const counters = document.querySelectorAll('.counter');
     counters.forEach((counter, index) => {
     counter.id = `inCart${index + 1}`;
     });
 };
 function changeCart(button, action){
     let tr = button.parentNode.parentNode;
-    let td = tr.querySelector(".Counter")
+    let td = tr.querySelector(".counter")
     let clicks = parseInt(td.value)
 
     if(action === "increment")
