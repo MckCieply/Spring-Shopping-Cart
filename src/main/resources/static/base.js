13a61b20d034c201da206e544c93127327439e5d
Alex <mwppl123@gmail.com>
2023-09-29 20:39:36 +0200

    Toggling buttons for form elements

--- src/main/resources/static/base.js
+++ src/main/resources/static/base.js
@@ -16,4 +16,20 @@ function changeCart(button, action){
         clicks -= 1;
     td.value = clicks.toString();
 
-};
+}
+
+// function onLoadToggleVisibility(){
+//     let toggleVisibilityButtons = document.getElementsByClassName("toggleButton");
+//     console.log(toggleVisibilityButtons)
+//     let toggleVisibilitySpan = document.getElementById("toggleSpan");
+//     let isItemVisible = false;
+
+    function toggleItemVisibility(button) {
+        let toggleSpan = button.parentNode.querySelector(".toggleSpan")
+        if (toggleSpan.style.display === "block") {
+            toggleSpan.style.display = "none"
+        } else {
+            toggleSpan.style.display = "block";
+        }
+    }
+
